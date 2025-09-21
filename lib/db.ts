83cb3576c579a0e2175c61d1495b0cffa0569663
gs6029907@gmail.com <gs6029907@gmail.com>
2025-09-21 19:21:20 +0530

    commit

--- lib/db.ts
+++ lib/db.ts
@@ -14,12 +14,11 @@ interface MongooseCache {
 
 // Extend NodeJS.Global to include mongoose cache
 declare global {
-  // eslint-disable-next-line no-var
   var mongoose: MongooseCache;
 }
 
 // Initialize cache
-let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+const cached: MongooseCache = global.mongoose || { conn: null, promise: null };
 global.mongoose = cached;
 
 export async function connectToDatabase() {
